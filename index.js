fa2bb53fcd5aa6c1d518b9f067b57312ec732ddf
linzhu <lin.lyn.zhu@gmail.com>
2022-03-17 23:13:38 -0700

    test passes

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const { fetchMyIP, fetchCoordsByIP} = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-
-//   fetchCoordsByIP(ip, (error, {latitude, longitude}) => {
-
-//     if (error) {
-//       console.log("It didn't work!" , error);
-//       return;
-//     }
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned IP:' , ip);
   
-//     console.log('It worked! Returned coordiates:' , {latitude, longitude});
-//   });
-// });
+  fetchCoordsByIP(ip, (error, {latitude, longitude}) => {
 
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+    console.log('It worked! Returned coordiates:' , {latitude, longitude});
+
+    fetchISSFlyOverTimes({latitude, longitude}, (error, passes) => {
+      if (error) {
+        console.log("It didn't work!" , error);
+        return;
+      }
+      console.log('It worked! Returned passes:' , passes);
+    });
+  });
+
+});
 
