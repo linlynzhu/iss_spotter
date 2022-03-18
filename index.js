1f14d37fcbc27019a9ed16d037d3549a3f78d9aa
linzhu <lin.lyn.zhu@gmail.com>
2022-03-17 23:50:10 -0700

    print the passes times (call a func that has callback fefined) in index.js file ; the nested test func moved to iss.js

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
+const {nextISSTimesForMyLocation} = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned IP:' , ip);
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
   
-  fetchCoordsByIP(ip, (error, {latitude, longitude}) => {
+//   fetchCoordsByIP(ip, (error, {latitude, longitude}) => {
 
-    if (error) {
-      console.log("It didn't work!" , error);
-      return;
-    }
-    console.log('It worked! Returned coordiates:' , {latitude, longitude});
+//     if (error) {
+//       console.log("It didn't work!" , error);
+//       return;
+//     }
+//     console.log('It worked! Returned coordiates:' , {latitude, longitude});
 
-    fetchISSFlyOverTimes({latitude, longitude}, (error, passes) => {
-      if (error) {
-        console.log("It didn't work!" , error);
-        return;
-      }
-      console.log('It worked! Returned passes:' , passes);
-    });
-  });
+//     fetchISSFlyOverTimes({latitude, longitude}, (error, passes) => {
+//       if (error) {
+//         console.log("It didn't work!" , error);
+//         return;
+//       }
+//       console.log('It worked! Returned passes:' , passes);
+//     });
+//   });
 
+// });
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
 });
-
